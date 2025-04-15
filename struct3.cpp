292e0d8cfdcad31f706ee1176c613908fa1d1589
Aldy0226 <putra.jaya.ft24@mail.umy.ac.id>
2025-04-15 17:01:22 +0700

    Menambahkan getline ke data

--- struct3.cpp
+++ struct3.cpp
@@ -6,3 +6,30 @@ struct detailAlamat{
     string kota;
 };
 
+struct mahasiswa{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
+
+int main(){
+    mahasiswa mhs;
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cin.ignore();
+    cout << "Masukkan Nama = ";
+    getline(cin , mhs.nama);
+    cout << "Masukkan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan Alamat Kota = ";
+    cin >> mhs.alamat.kota;
+
+    cout << endl;
+    cout << "Data Mahasiswa ke-" << endl;
+   
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat Desa = " << mhs.alamat.desa << endl;
+    cout << "Alamat Kota = " << mhs.alamat.kota << endl;
+}
