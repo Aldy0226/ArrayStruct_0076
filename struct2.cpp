ecaf26d34489dcedcf94b829f410b0d0c8b672a5
Aldy0226 <putra.jaya.ft24@mail.umy.ac.id>
2025-04-15 16:59:57 +0700

    Menambahkan data di struct mahasiswa

--- struct2.cpp
+++ struct2.cpp
@@ -6,3 +6,29 @@ struct detailAlamat{
     string kota;
 };
 
+struct mahasiswa{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
+
+int main(){
+    mahasiswa mhs;
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan Nama = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan Alamat Kota = ";
+    cin >> mhs.alamat.kota;
+
+    cout << endl;
+    cout << "Data Mahasiswa ke-" << endl;
+   
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat Desa = " << mhs.alamat.desa << endl;
+    cout << "Alamat Kota = " << mhs.alamat.kota << endl;
+}
