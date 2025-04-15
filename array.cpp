9efb9b06545becdcfb420ec7defa25f90f2c4292
Aldy0226 <putra.jaya.ft24@mail.umy.ac.id>
2025-04-15 16:52:00 +0700

    Mengisi dan menampilkan array

--- array.cpp
+++ array.cpp
@@ -8,3 +8,22 @@ int skor[5] = {8,12,18,5};
 //deklarasi array dengan ukuran dan tidak ada nilai awal
 string nama[3];
 
+int main(){
+    cout << "Skor ke 3 = " << skor[2] << endl;
+    //mengisi array pada lokasi/index tertentu
+    skor[2] = 25;
+    cout << "Skor ke 2 = " << skor[2] << endl;
+    
+    //mengisi array dengan loop for
+    for(int i = 0; i < 3; i++){
+        cout << "Nama Mahasiswa ke-" << i+1 << " = ";
+        cin >> nama[i];
+    }
+    
+    cout<<endl;
+    //menampilkan array dengan loop for
+    for(int i = 0; i < 3; i++){
+        cout << "Data Mahasiswa ke-" << i+1 << " = " << nama[i]
+        << endl;
+    }
+}
