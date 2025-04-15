487ae3c84859d8a8649c8c39a9642ca747769edb
Aldy0226 <putra.jaya.ft24@mail.umy.ac.id>
2025-04-15 16:58:20 +0700

    Membuat struct mahasiswa

--- struct.cpp
+++ struct.cpp
@@ -7,3 +7,18 @@ struct mahasiswa{
     string alamat;
 };
 
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim ="20240140001";
+    mhs.nama ="Abra";
+    mhs.alamat ="Kasihan";
+    
+    cout << "Masukan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukan Nama = ";
+    cin >> mhs.nama;
+    cout << "Masukan Alamat =";
+    cin >> mhs.alamat;
+
+}
